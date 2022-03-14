1f5269a892df877214b1f01e0ea63f11927090aa
luuthieukhue <luukhue0506@gmail.com>
2022-03-14 11:34:05 +0700

    first commit

---
+++ lab3-luuthieukhue/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab3_luuthieukhue.Startup))]
+namespace lab3_luuthieukhue
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
